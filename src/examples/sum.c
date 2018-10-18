2a09ab1c85dfa0254944fa3344c835b4647b6ea6
JungYeon <sjy20131565@gmail.com>
2018-10-18 13:31:47 -0700

    modify sum.c

--- src/examples/sum.c
+++ src/examples/sum.c
@@ -5,8 +5,7 @@
 int
 main(int argc, char **argv)
 {
-    printf("%d ", fibonacci(atoi(argv[1])));
-	printf("%d\n", sum_of_four_integers(atoi(argv[1]), atoi(argv[2]), 
-										atoi(argv[3]), atoi(argv[4])));
+	printf("%d ", fibonacci(atoi(argv[1])));
+	printf("%d\n", sum_of_four_integers(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4])));
 	return EXIT_SUCCESS;
 }
