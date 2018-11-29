3f79865aefc5471468dcb82a0275ef588d011222
JungYeon <sjy20131565@gmail.com>
2018-11-29 02:27:31 -0800

    remove fixed-point files

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -59,6 +59,28 @@ static unsigned thread_ticks;   /* # of timer ticks since last yield. */
 bool thread_prior_aging;
 #endif
 
+fixed32_t fraction = 2 << 14;
+fixed32_t half_fraction = 2 << 13;
+fixed32_t load_avg;
+
+fixed32_t int2fixed(int x) {return x * fraction;}
+int fixed2int(fixed32_t x)
+{
+    if(x < 0) return (x - half_fraction) / fraction;
+    else return (x + half_fraction) / fraction;
+}
+int fixed2int_trunc(fixed32_t x) {return x / fraction;}
+
+fixed32_t fixed_add(fixed32_t x, fixed32_t y) {return x + y;}
+fixed32_t fixed_sub(fixed32_t x, fixed32_t y) {return x - y;}
+fixed32_t fixed_mul(fixed32_t x, fixed32_t y) {return (((int64_t)x) * y) / fraction;}
+fixed32_t fixed_div(fixed32_t x, fixed32_t y) {return (((int64_t)x) * fraction) / y;}
+
+fixed32_t fixed_add_int(fixed32_t x, int y) {return x + y * fraction;}
+fixed32_t fixed_sub_int(fixed32_t x, int y) {return x - y * fraction;}
+fixed32_t fixed_mul_int(fixed32_t x, int y) {return x * y;}
+fixed32_t fixed_div_int(fixed32_t x, int y) {return x / y;}
+
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "-o mlfqs". */
@@ -219,6 +241,9 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
+
+  //if(thread_current()->priority < priority)
+    //thread_yield();
 
   return tid;
 }
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -10,6 +10,27 @@
 /* Project #3. */
 extern bool thread_prior_aging;
 #endif
+
+typedef int32_t fixed32_t;
+
+extern fixed32_t fraction;
+extern fixed32_t half_fraction;
+extern fixed32_t load_avg;
+
+fixed32_t int2fixed(int x);
+int fixed2int(fixed32_t x);
+int fixed2int_trunc(fixed32_t x);
+
+fixed32_t fixed_add(fixed32_t x, fixed32_t y);
+fixed32_t fixed_sub(fixed32_t x, fixed32_t y);
+fixed32_t fixed_mul(fixed32_t x, fixed32_t y);
+fixed32_t fixed_div(fixed32_t x, fixed32_t y);
+
+fixed32_t fixed_add_int(fixed32_t x, int y);
+fixed32_t fixed_sub_int(fixed32_t x, int y);
+fixed32_t fixed_mul_int(fixed32_t x, int y);
+fixed32_t fixed_div_int(fixed32_t x, int y);
+
 /* States in a thread's life cycle. */
 enum thread_status
   {
@@ -112,6 +133,8 @@ struct thread
 
     int64_t wakeup_time;
     struct list_elem sleep_elem;
+    int nice;
+    fixed32_t recent_cpu;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
